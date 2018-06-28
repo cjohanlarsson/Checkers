1ac50418d418be6c30e3247604fc617b05f9408c
Carl Larsson <cjohanlarsson@gmail.com>
2018-06-28 11:21:38 -0700

    Initial game board and display

---
+++ Assets/Scripts/Gameplay/Game.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkers.Gameplay
+{
+	public struct Position
+	{
+		public int x;
+		public int y;
+	}
+
+	public enum Team
+	{
+		Empty,
+		X,
+		O
+	}
+
+	public struct CellState
+	{
+		public Team team;
+	}
+
+	public enum MoveError
+	{
+		Success,
+		Unknown
+	}
+
+	public class Move
+	{
+
+		public List<Gameplay.Position> positions;
+
+		public Move() 
+		{
+			positions = new List<Position>();
+		}
+
+		/// <summary>
+		/// Parse a move in the format X1,Y1 X2,Y2 X3,Y3
+		/// </summary>
+		/// <returns><c>true</c>, if parse was tryed, <c>false</c> otherwise.</returns>
+		/// <param name="input">Input.</param>
+		/// <param name="move">Move.</param>
+		public static bool TryParse(string input, ref Move move)
+		{
+			Move newMove = new Move();
+
+			var arr = input.Split(' ');
+			foreach(var a in arr)
+			{
+				var arr2 = a.Split(',');
+				if (arr2.Length != 2)
+					return false;
+
+				int x; int y;
+				if(int.TryParse(arr2[0], out x) && int.TryParse(arr2[1], out y) )
+				{
+					newMove.positions.Add(new Position() { x = x, y = y });
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			move = newMove;
+			return true;
+		}
+	}
+
+	public class Game
+	{
+		public CellState[,] board;
+
+		public Game(int boardSize) {
+			board = new CellState[boardSize, boardSize];
+		}
+
+		public Team CurrentTurn { get; private set; }
+
+		public MoveError PlayMove(Move move)
+		{
+			return MoveError.Unknown;
+		}
+
+		public CellState GetStateAt(int x, int y)
+		{
+			if (x < 0 || y < 0 || x >= board.GetLength(0) || y >= board.GetLength(1))
+				throw new System.IndexOutOfRangeException("Not a valid position!");
+			
+			return board[x, y];
+		}
+
+		public string BoardToString()
+		{
+			var sb = new StringBuilder();
+			for (int y = board.GetLength(1) - 1; y >= 0; y--) 
+			{
+				sb.Append(y);
+				sb.Append(' ');
+				for (int x = 0; x < board.GetLength(0); x++) 
+				{
+					var state = board[x, y];
+					sb.Append(state.team == Team.Empty ? '.' : (state.team == Team.O ? 'O' : 'X'));
+					sb.Append(' ');
+				}
+				sb.Append('\n');
+			}
+
+			sb.Append("  ");
+			for (int x = 0; x < board.GetLength(0); x++)
+			{
+				sb.Append(x);
+				sb.Append(' ');
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ Assets/Scripts/Ux/Manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Checkers.Ux
+{
+	public class Manager : MonoBehaviour
+	{
+		[SerializeField] Text consoleBoard;
+		[SerializeField] InputField consoleInput;
+		[SerializeField] Button consoleSubmit;
+		[SerializeField] Text consoleMessage;
+
+		Gameplay.Game game;
+
+		void Awake()
+		{
+			game = new Gameplay.Game(8);
+
+			consoleSubmit.onClick.AddListener(() =>
+			{
+				Gameplay.Move move = null;
+				if( Gameplay.Move.TryParse(consoleInput.text, ref move) ) 
+				{
+					var err = game.PlayMove(move);
+					if(err != Gameplay.MoveError.Success)
+					{
+						DisplayErrorMessage(err);
+					}
+					else
+					{
+						RefreshGameView();
+					}
+				}
+				else
+				{
+					DisplayErrorMessage("Couldn't parse moves!");
+				}
+			});
+
+			ClearErrorMessage();
+			RefreshGameView();
+		}
+
+
+		static readonly Dictionary<Gameplay.MoveError, string> moveErrorMessages = new Dictionary<Gameplay.MoveError, string>()
+		{
+			{Gameplay.MoveError.Success, "Success!"},
+			{Gameplay.MoveError.Unknown, "Unknown!"}
+		};
+
+		void RefreshGameView()
+		{
+			consoleBoard.text = game.BoardToString();
+		}
+
+		void DisplayErrorMessage(Gameplay.MoveError errorMsg)
+		{
+			consoleMessage.text = moveErrorMessages[errorMsg];
+		}
+
+		void DisplayErrorMessage(string errorMsg)
+		{
+			consoleMessage.text = errorMsg;
+		}
+
+		void ClearErrorMessage()
+		{
+			consoleMessage.text = "";
+		}
+	}
+}
